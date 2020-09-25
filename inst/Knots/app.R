001a81a470a83704953f1bfa269ffa7f33c6a647
Sophie Huebler <shuebler@ou.edu>
2020-09-25 13:35:27 -0500

    Removing rootsolve dependency

--- inst/Knots/app.R
+++ inst/Knots/app.R
@@ -2,7 +2,7 @@
 library(shiny)
 library(ggplot2)
 library(purrr)
-library(rootSolve)
+#library(rootSolve)
 library(readr)
 library(plotly)
 source("pieceFunctions.R")
@@ -17,12 +17,12 @@ d = spruce.df$BHDiameter
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
-  
+
   # Application title
   titlePanel("Spruce Data Set: Piecewise Regression"),
-  
-  # Sidebar with a slider input for number of bins 
-  
+
+  # Sidebar with a slider input for number of bins
+
   sidebarLayout(
     sidebarPanel(
       sliderInput("xk",
@@ -38,24 +38,24 @@ ui <- fluidPage(
                     max = max(d),
                     value = 18,
                     step=0.01),
-      
+
       sliderInput("xk_int",
                   "choose L and U for the first knot:",
                   min = min(d),
                   max = max(d),
                   value = c(9,14),
                   step=0.01),
-      
+
       sliderInput("xk2_int",
                   "choose L and U for the second knot:",
                   min = min(d),
                   max = max(d),
                   value = c(16,22),
                   step=0.01),
-      
-      
+
+
     ),
-    
+
     # Show a plot of the generated distribution
     mainPanel(
       plotOutput("regressPlot"),
@@ -68,38 +68,38 @@ ui <- fluidPage(
 
 # Define server logic required to draw a histogram
 server <- function(input, output, session) {
-  
-  # Plot that shows curve, interactive points, and points which give the max R value. 
+
+  # Plot that shows curve, interactive points, and points which give the max R value.
   #Also displays current R value
   output$regressPlot <- renderPlot({
     plot(spruce.df,main="Piecewise regression",pch=21,bg="black")
-    
+
 
     tmp<- spruce.df %>%
       mutate(X=(BHDiameter-input$xk)*(BHDiameter>input$xk))%>%
       mutate(X2=(BHDiameter-input$xk2)*(BHDiameter>input$xk2)) %>%
       lm(Height~BHDiameter + X + X2, data=.)%>%
       summary()
-    
-    
+
+
     curve(twoKnotReg(x,xk=input$xk, xk2=input$xk2, coef=tmp$coefficients[,"Estimate"] ),
         add=TRUE,
        lwd=2,
       col="Blue")
-    
+
      points(input$xk,twoKnotReg(input$xk,input$xk, input$xk2, coef=tmp$coefficients[,"Estimate"] ),col="black",pch=21,bg="green",cex=2)
      points(input$xk2,twoKnotReg(input$xk2,input$xk, input$xk2, coef=tmp$coefficients[,"Estimate"] ),col="black",pch=21,bg="green",cex=2)
-     
+
      points(maxR2$par[1],twoKnotReg(maxR2$par[1],maxR2$par[1], maxR2$par[2],coef=maxEst ),col="black",pch=21,bg="purple",cex=2)
      points(maxR2$par[2],twoKnotReg(maxR2$par[2],maxR2$par[1], maxR2$par[2],coef=maxEst ),col="black",pch=21,bg="purple",cex=2)
-     
-     
+
+
      text((input$xk+input$xk2)/2,14,
           paste("R sq.=",round(tmp$r.squared,4) ))
-     
-   }) 
-  
-  
+
+   })
+
+
   maxR2=optim(c(5,22), rsq, data=spruce.df, control=c(fnscale=-1))
   maxEst=coeff(maxR2$par[1], maxR2$par[2],spruce.df)
 
@@ -115,20 +115,20 @@ server <- function(input, output, session) {
     s=table(Knot_1=maxR2$par[1], Knot_2=maxR2$par[2], Max_R2_Value=rsq(maxR2$par, spruce.df))
     s
   })
-  
+
   #Second knot depends on the first
   observe({
   xk2min<-input$xk+0.01
   updateSliderInput(session, "xk2", min=xk2min)
   })
 
-# Second interval depends on the first 
+# Second interval depends on the first
 observe({
   xk2IntMin<-input$xk_int[2]
   updateSliderInput(session, "xk2_int", min=xk2IntMin)
 })
 }
 
-# Run the application 
+# Run the application
 shinyApp(ui = ui, server = server)
 
